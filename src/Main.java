c36125a167afe4b9d583f649baa805a6ffaf47a8
OliwierBul <oliwiermdo@gmail.com>
2025-10-27 13:17:57 +0100

    deklaracja klasy

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Osoba osoba = new Osoba();
+        System.out.println(osoba.imie);
+        osoba.imie = "Jan";
+        System.out.println(osoba.imie);
+    }
+}
